e72281a88f0c24b46c3442f39e4c2d920b36dd19
wayne <waynecchen@liontravel.com>
2024-02-22 15:57:13 +0800

    first commit

---
+++ storeapi/logging_conf.py
@@ -0,0 +1,57 @@
+from logging.config import dictConfig
+
+from storeapi.config import DevConfig, config
+
+
+def configure_logging() -> None:
+    dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "filter": {
+                "correlation_id": {
+                    "()": "asgi_correlation_id.CorrelationIdFilter",
+                    "uuid_length": 8 if isinstance(config, DevConfig) else 32,
+                    "default_value": "-",
+                }
+            },
+            "formatters": {
+                "console": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H-%M:%S",
+                    "format": "%(name)s%(lineno)d - %(message)s",
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H-%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s%(lineno)d - %(message)s",
+                },
+            },
+            "handlers": {
+                "default": {
+                    "class": "rich.logging.RichHandler",
+                    "level": "DEBUG",
+                    "formatter": "console",
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "storeapi.log",
+                    "maxBytes": 1024 * 1024,  # 1MB
+                    "backupCount": 5,
+                    "encoding": "utf8",
+                },
+            },
+            "loggers": {
+                "uvicorn": {"handlers": ["default", "rotating_file"], "level": "INFO"},
+                "storeapi": {
+                    "handlers": ["default", "rotating_file"],
+                    "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
+                    "propagate": False,
+                },
+                "databases": {"handlers": ["default"], "level": "WARNING"},
+                "aiosqlite": {"handlers": ["default"], "level": "WARNING"},
+            },
+        }
+    )
