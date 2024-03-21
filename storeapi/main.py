cc150b49437de00403a3b346d2fbaf786cbffe56
WayneChen <lenrr811@gmail.com>
2024-03-21 17:41:41 +0800

    240321

--- storeapi/main.py
+++ storeapi/main.py
@@ -4,10 +4,10 @@ from contextlib import asynccontextmanager
 from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI, HTTPException
 from fastapi.exception_handlers import http_exception_handler
-
 from storeapi.database import database
 from storeapi.logging_conf import configure_logging
 from storeapi.routers.posts import router as post_router
+from storeapi.routers.user import router as user_router
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +25,7 @@ app.add_middleware(CorrelationIdMiddleware)
 
 
 app.include_router(post_router)
+app.include_router(user_router)
 
 
 @app.exception_handler(HTTPException)
--- storeapi/tests/conftest.py
+++ storeapi/tests/conftest.py
@@ -4,9 +4,10 @@ from typing import AsyncGenerator, Generator
 import pytest
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
+from httpx._transports.asgi import ASGITransport
 
 os.environ["ENV_STATE"] = "test"
-from storeapi.database import database  # noqa: E402
+from storeapi.database import database, user_table  # noqa: E402
 from storeapi.main import app  # noqa: E402
 
 
@@ -28,7 +29,36 @@ async def db() -> AsyncGenerator:
     await database.disconnect()
 
 
+# @pytest.fixture()
+# async def async_client(client) -> AsyncGenerator:
+#     async with AsyncClient(app=app, base_url=client.base_url) as ac:
+#         yield ac
+
+
 @pytest.fixture()
 async def async_client(client) -> AsyncGenerator:
-    async with AsyncClient(app=app, base_url=client.base_url) as ac:
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url=client.base_url
+    ) as ac:
         yield ac
+
+
+# @pytest.fixture()
+# async def async_client_wsgi(client) -> AsyncGenerator:
+#     async with AsyncClient(
+#         transport=WSGITransport(app=app), base_url=client.base_url
+#     ) as ac:
+#         yield ac
+
+
+@pytest.fixture()
+async def registered_user(async_client: AsyncClient) -> dict:
+    user_details = {"email": "test@example.net", "password": "1234"}
+
+    await async_client.post("/register", json=user_details)
+
+    query = user_table.select().where(user_table.c.email == user_details["email"])
+    user = await database.fetch_one(query)
+    user_details["id"] = user.id
+
+    return user_details
